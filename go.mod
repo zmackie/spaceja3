90ac3c791d19dcbffad2324c1d0d7c93bd468a15
Zander Mackie <zmackie@gmail.com>
2023-06-20 10:15:00 -0400

    basic skeleton

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.20
 require (
 	github.com/Microsoft/go-winio v0.4.9 // indirect
 	github.com/apache/thrift v0.16.0 // indirect
+	github.com/dreadl0ck/ja3 v1.0.4 // indirect
+	github.com/dreadl0ck/tlsx v1.0.1-google-gopacket // indirect
+	github.com/google/gopacket v1.1.18 // indirect
 	github.com/osquery/osquery-go v0.0.0-20230603132358-d2e851b3991b // indirect
 	github.com/pkg/errors v0.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/osquery/osquery-go"
 	"github.com/osquery/osquery-go/plugin/table"
@@ -26,6 +27,7 @@ func main() {
 	// table.NewPlugin requires the table plugin name,
 	// a slice of Columns and a Generate function.
 	server.RegisterPlugin(table.NewPlugin("foobar", FoobarColumns(), FoobarGenerate))
+	spawn_caps()
 	if err := server.Run(); err != nil {
 		log.Fatalln(err)
 	}
@@ -52,4 +54,25 @@ func FoobarGenerate(ctx context.Context, queryContext table.QueryContext) ([]map
 			"baz": "baz",
 		},
 	}, nil
+}
+
+func spawn_caps() {
+	// loop through interfaces
+	// spawn a capture for each interface
+	http_events()
+}
+
+type ja3Sig struct {
+}
+
+var eventsMutex sync.Mutex
+var events []ja3Sig
+
+func http_events() {
+	//# Event loop that does pcap
+	//# sends ja3/ja3s hashes to a channelcontext
+	//# channel retrieves them when table is looked up
+	eventsMutex.Lock()
+	defer eventsMutex.Unlock()
+	events = append(events, ja3Sig{})
 }
